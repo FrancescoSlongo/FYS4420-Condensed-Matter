bdf1495969e36ab5d166ac2d57701bb29ab565a0
FrancescoSlongo <Slongofrancesco@gmail.com>
2018-09-19 12:05:44 +0200

    Update 19/09/18 12:05
    Sbagliato cartella di modifica dei codici, dovrebbero essere aggiornati ora.

--- DATI PID/Code/Data_analysis.m
+++ DATI PID/Code/Data_analysis.m
@@ -18,8 +18,9 @@ dTinf_2 = std(T(1,6000:10800));
 clear T;
 
 %% Task 5-6 (calcolo H0, T1, T2)
-load('task_5_temp_check_1.mat');
+load('task_5.mat');
 % parametri
+t = 0.6*[1:length(T)]; %[s]
 Vpulse = 3.5; % [V]
 Vstep = 3;
 eps = 18; % [s] +- 1s
@@ -32,27 +33,11 @@ dH0_2 = sqrt(dTinf_2^2 + dTroom^2)/Vstep;
 
 % Trovare il punto di massimo
 figure();
-plot(cm(1,1:300),T, '.', 'markersize', 6,'color', 'r');
-% I get the axes and put some labels
-axs1 = gca;
-set(axs1, 'xtick', 0:25:200);
-set(axs1, 'ytick', 0.5:0.5:3);
-set(axs1, 'xlim', [0 200]);
-set(axs1, 'ylim', [0.5 3]);
-xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
-% Grid
-grid on;
+plot(t,T);
 
 % coordinate del punto di massimo
 rp = 2.62816; % trovare a occhio e inserire a mano
-tp = 27.05;
+tp = 27.6;
 
 % Funzione rp = f(T12)
 % vettore con T12
@@ -61,32 +46,44 @@ T12 = [1,0:0.1:400];
 rp_func = Vpulse*H0_1.*exp(-tp./T12).*(exp(eps./T12) - 1);
 % plot della funzione con la retta di intersezione
 figure();
-plot(T12,rp_func, '.', 'markersize', 6,'color', 'r');
+plot(T12,rp_func);
 hold on
-plot(T12,rp.*ones(1, length(T12)), '.', 'markersize', 6,'color', 'b');
-% I get the axes and put some labels
-axs1 = gca;
-set(axs1, 'xtick', 0:50:400);
-set(axs1, 'ytick', 0:1:12);
-set(axs1, 'xlim', [0 400]);
-set(axs1, 'ylim', [0 12]);
-xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
-% Grid
-grid on;
+plot(T12,rp.*ones(1, length(T12)));
 
-%% Trovare a occhio T1 e T2
-T1 = 190.81;
-T2 = 3.6977;
+% Trovare a occhio T1 e T2
+T1 = 3.941;
+T2 = 188.304;
 
 %% Task 7
 P=(1/3)*((T1+T2)^2)/(T1*T2)-1;
 I=(1/27)*((T1+T2)^3)/((T1*T2)^2);
 Kp=P/H0_1;
+dKp = P/H0_1^2*dH0_1;
 Ki=I/H0_1;
+dKi = I/H0_1^2*dH0_1;
+%% Task 12
+% Calcolo il rapporto fra i comportamenti asintotici
+delete T cm Tref
+load('task_12_pid_control.mat');
+% Rapporto sperimentale prima reference temperature
+Tas1 = mean(T(327:483)) - Troom;
+dTas1 = sqrt(std(T(327:483))^2+dTroom^2);
+Tref1 = Tref(327) - Troom;
+dTref1 = dTroom;
+Rapp1 = Tas1/Tref1;
+dRapp1 = sqrt((dTas1/Tref1)^2 + ((Tas1/Tref1^2)*dTref1)^2);
+% Rapporto sperimentale seconda reference temperature
+Tas2 = mean(T(1183:1500)) - Troom;
+dTas2 = sqrt(std(T(1183:1500))^2+dTroom^2);
+Tref2 = Tref(1400) - Troom;
+dTref2 = dTroom;
+Rapp2 = Tas2/Tref2;
+dRapp2 = sqrt((dTas2/Tref2)^2 + ((Tas2/Tref2^2)*dTref2)^2);
+% Rapporto teorico
+RapTeo = Kp/(H0_1^(-1) + Kp);
+dRapTeo = sqrt((H0_1/(1+Kp*H0_1)^2*dKp)^2+(Kp/(1+Kp*H0_1)^2*dH0_1)^2);
+% Residui per compatibilità
+Res1 = abs(Rapp1 - RapTeo);
+dRes1 = sqrt(dRapp1^2+dRapTeo^2);
+Res2 = abs(Rapp2 - RapTeo);
+dRes2 = sqrt(dRapp2^2+dRapTeo^2);
--- DATI PID/Code/Plotter.m
+++ DATI PID/Code/Plotter.m
@@ -1,29 +1,24 @@
 %% Script for plotting the data in the PID control experience
 zzz
 load('task_3_temp_check.mat');
-cm = cm*0.6;
 % I create the figure for the task 3
 figure();
 load('task_3_temp_check.mat');
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:75:1260);
+set(axs1, 'xtick', 0:125:2100);
 set(axs1, 'ytick', 295.97:0.035:296.2);
-set(axs1, 'xlim', [0 1260]);
+set(axs1, 'xlim', [0 2100]);
 set(axs1, 'ylim', [295.97 296.2]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Zoom: I create new axes
-axes('position', [.68 .17 .2 .2])
+axes('position', [.6 .2 .2 .2])
 box on
 % I Plot the same graph but smaller
 plot(cm(1:200), T(1:200), '.','markersize',6,'color', 'r');
@@ -32,22 +27,17 @@ clc;
 clear variables;
 figure();
 load('task_4_temp_check.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:350:3780);
+set(axs1, 'xtick', 0:650:10500);
 set(axs1, 'ytick', 304.5:2:324);
-set(axs1, 'xlim', [0 3780]);
+set(axs1, 'xlim', [0 10500]);
 set(axs1, 'ylim', [304.5 324]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Zoom: I create new axes
@@ -57,28 +47,23 @@ box on
 plot(cm(10000:11405), T(10000:11405), '.','markersize',6,'color', 'r');
 % I set the limits
 zoom_axes1 = gca;
-set(zoom_axes1, 'xlim',[3070, 3680] , 'ylim', [322.2, 322.8])
+set(zoom_axes1, 'xlim',[8600, 10200] , 'ylim', [322.2, 322.8])
 %% I plot the figure for task 4 - second attempt
 clc;
 clear variables;
 figure();
 load('task_4_1_temp_check.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:500:6600);
-set(axs1, 'ytick', 301:2.5:325);
-set(axs1, 'xlim', [0 6600]);
+set(axs1, 'xtick', 0:650:11000);
+set(axs1, 'ytick', 301:2:325);
+set(axs1, 'xlim', [0 11000]);
 set(axs1, 'ylim', [301 325]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Zoom: I create new axes
@@ -88,28 +73,25 @@ box on
 plot(cm(10000:12000), T(10000:12000), '.','markersize',6,'color', 'r');
 % I set the limits
 zoom_axes1 = gca;
-set(zoom_axes1, 'xlim',[5100, 6510] , 'ylim', [323.76, 324.05])
+set(zoom_axes1, 'xlim',[8500, 10850] , 'ylim', [323.76, 324.05])
 %% I plot the figure for task 5
 clc;
 clear variables;
 figure();
 load('task_5_temp_check_1.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
+hold on
+plot(cm(30:31), [0, 1000], 'color', 'b', 'LineStyle','--', 'linewidth', 1.5);
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:10:114);
+set(axs1, 'xtick', 0:20:190);
 set(axs1, 'ytick', 297:0.25:298.8);
-set(axs1, 'xlim', [0 114]);
+set(axs1, 'xlim', [0 190]);
 set(axs1, 'ylim', [297 298.8]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Zoom: I create new axes
@@ -119,34 +101,35 @@ box on
 plot(cm(35:60), T(35:60), '.','markersize',6,'color', 'r');
 % I set the limits
 zoom_axes1 = gca;
-set(zoom_axes1, 'xlim',[12, 20] , 'ylim', [298.5, 298.73])
+set(zoom_axes1, 'xlim',[20, 34] , 'ylim', [298.5, 298.73])
 %% I plot the figure for task 9
 clc;
 clear variables;
 figure();
 load('task_9_pid_control.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 hold on;
 plot(cm, Tref,'color', 'b');
+T1 = 3.941;
+T2 = 188.304;
+Tau = 1/(1/3 * (1/T1+1/T2));
+theo = 296.1 + 7.05*((1-1.5*Tau/(T1+T2))*(cm/Tau).^2 - cm/Tau - 1).*exp(-cm/Tau) + 7.05;
+hold on;
+plot(cm, theo,'color', 'g','linewidth', 1.5);
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:30:408);
+set(axs1, 'xtick', 0:50:680);
 set(axs1, 'ytick', 297:0.75:307);
-set(axs1, 'xlim', [0 408]);
+set(axs1, 'xlim', [0 680]);
 set(axs1, 'ylim', [297 307]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Legend
-legend('Data','Reference temperature','location','northeast');
+legend('Data','Reference temperature','Theorical model','location','northeast');
 % Zoom: I create new axes
 axes('position', [.6 .2 .25 .25])
 box on
@@ -156,30 +139,25 @@ hold on
 plot(cm(700:1000), Tref(700:1000),'color', 'b');
 % I set the limits
 zoom_axes1 = gca;
-set(zoom_axes1, 'xlim',[270, 402] , 'ylim', [303.1, 303.21])
+set(zoom_axes1, 'xlim',[450, 607] , 'ylim', [303.1, 303.21])
 %% I plot the figure for task 10
 clc;
 clear variables;
 figure();
 load('task_10_1_pid_control.mat');
-cm = cm*0.6;
 r10 = plot(cm, T, '.', 'markersize', 6,'color', 'r');
 hold on;
 b10 = plot(cm, Tref,'color', 'b');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:50:660);
+set(axs1, 'xtick', 0:100:1100);
 set(axs1, 'ytick', 299.5:1.5:320);
-set(axs1, 'xlim', [0 660]);
+set(axs1, 'xlim', [0 1100]);
 set(axs1, 'ylim', [299.5 320]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Legend
@@ -189,24 +167,19 @@ clc;
 clear variables;
 figure();
 load('task_11_1_pid_control.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 hold on;
 plot(cm, Tref,'color', 'b');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:60:560);
+set(axs1, 'xtick', 0:100:904);
 set(axs1, 'ytick', 302.5:1.5:320);
-set(axs1, 'xlim', [0 560]);
+set(axs1, 'xlim', [0 940]);
 set(axs1, 'ylim', [302.5 320]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Legend
@@ -216,24 +189,19 @@ clc;
 clear variables;
 figure();
 load('task_12_pid_control.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 hold on;
 plot(cm, Tref,'color', 'b');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:50:645);
+set(axs1, 'xtick', 0:100:1075);
 set(axs1, 'ytick', 302:1:313.5);
-set(axs1, 'xlim', [0 645]);
+set(axs1, 'xlim', [0 1075]);
 set(axs1, 'ylim', [302 313.5]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);
-xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Legend
@@ -243,23 +211,19 @@ clc;
 clear variables;
 figure();
 load('task_13_pid_control.mat');
-cm = cm*0.6;
 plot(cm, T, '.', 'markersize', 6,'color', 'r');
 hold on;
 plot(cm, Tref,'color', 'b');
 % I get the axes and put some labels
 axs1 = gca;
-set(axs1, 'xtick', 0:30:400);
+set(axs1, 'xtick', 0:50:667);
 set(axs1, 'ytick', 301.9:0.2:303.57);
-set(axs1, 'xlim', [0 400]);
+set(axs1, 'xlim', [0 667]);
 set(axs1, 'ylim', [301.9 303.57]);
 xl = xlabel('$t [s]$', 'interpreter', 'latex');
-set(xl, 'FontSize', 16);
-yl = ylabel('$T [K]$', 'interpreter', 'latex');
-set(yl, 'FontSize', 16);xt = get(gca, 'xtick');
-set(gca, 'FontSize', 16);
-yt = get(gca, 'ytick');
-set(gca, 'FontSize', 16);
+set(xl, 'FontSize', 12);
+yl = ylabel('T [K]', 'interpreter', 'latex');
+set(yl, 'FontSize', 12);
 % Grid
 grid on;
 % Legend
@@ -273,4 +237,4 @@ hold on
 plot(cm(700:800), Tref(700:800),'color', 'b');
 % I set the limits
 zoom_axes1 = gca;
-set(zoom_axes1, 'xlim',[270, 312] , 'ylim', [302.8, 303.6]);
+set(zoom_axes1, 'xlim',[450, 520] , 'ylim', [302.8, 303.6]);
